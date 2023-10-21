966d94a7751a295f4ea5d7966b32282ac92c97f4
Dominik Szamburski <dominikszamburski99@gmail.com>
2023-10-21 09:50:46 +0200

    test: add generic TestCase

---
+++ tests/TestCase.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Nulldark\Tests;
+
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use Mockery\MockInterface;
+
+class TestCase extends \PHPUnit\Framework\TestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    /**
+     * Configures and returns a mock object with proper type-hinting
+     *
+     * @param class-string<T> $class
+     * @param mixed ...$arguments
+     *
+     * @return T & MockInterface
+     * @template T
+     */
+    public function mock(string $class, mixed ...$arguments): MockInterface
+    {
+        /** @var T & MockInterface */
+        return \Mockery::mock($class, $arguments);
+    }
+
+    /**
+     * Configures and returns a mock object for spying with proper type-hinting
+     *
+     * @param class-string<T> $class
+     * @param mixed ...$arguments
+     *
+     * return T & MockInterface
+     *
+     * @template T
+     */
+    public function spy(string $class, mixed ...$arguments): MockInterface
+    {
+        /** @var T & MockInterface */
+        return Mockery::spy($class, ...$arguments);
+    }
+}
