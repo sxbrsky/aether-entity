111a98a7844e6e2f565ece052bd702d8b3115570
Dominik Szamburski <dominikszamburski99@gmail.com>
2023-10-25 23:54:25 +0200

    tests: adding dummies classes

---
+++ tests/Stubs/DummyClass.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Nulldark\Tests\Stubs;
+
+use Nulldark\ORM\Mapping\Annotations;
+
+#[Annotations\Entity]
+class DummyClass
+{
+
+}
---
+++ tests/Stubs/DummyClassExtended.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Nulldark\Tests\Stubs;
+
+class DummyClassExtended extends DummyClass
+{
+
+}
